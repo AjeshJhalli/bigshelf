bb2fc8d6a8b020043cac09cac5798532c0fdf740
Ajesh Jhalli <ajeshjhalli@mail.com>
2024-06-16 22:53:57 +0100

    Use Deno.env in one place

--- routers/routerPerson.tsx
+++ routers/routerPerson.tsx
@@ -1,30 +1,28 @@
-import { render } from 'https://cdn.skypack.dev/preact-render-to-string@v5.1.12';
-import { Router } from 'jsr:@oak/oak/router';
+import { render } from "https://cdn.skypack.dev/preact-render-to-string@v5.1.12";
+import { Router } from "jsr:@oak/oak/router";
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
-import Person from '../pages/person/Person.tsx';
-import r from '../utils/r.jsx';
-import getPerson from './getPerson.ts';
-import PersonForm from '../pages/person/PersonForm.jsx';
+import Person from "../pages/person/Person.tsx";
+import r from "../utils/r.jsx";
+import getPerson from "./getPerson.ts";
+import PersonForm from "../pages/person/PersonForm.jsx";
 
 const env = await load();
-const databaseUrl = env["DATABASE_URL"];
+const databaseUrl = Deno.env.get("DATABASE_URL");
 const pool = new Pool(databaseUrl, 3, true);
 const connection = await pool.connect();
 
 const routerPerson = new Router();
 
 routerPerson
-  .get('/', async (context) => {
-
+  .get("/", async (context) => {
     const personId = parseInt(context.params.personId);
 
     if (!personId) {
-      context.response.body = 'Bad request';
+      context.response.body = "Bad request";
       context.response.status = 418;
     } else {
-
       const person = await getPerson(connection, personId);
 
       try {
@@ -35,28 +33,26 @@ routerPerson
         context.response.status = 500;
       }
     }
-
   })
-  .post('/', async (context) => {
-
+  .post("/", async (context) => {
     const personId = parseInt(context.params.personId);
 
     const data = await context.request.body.form();
 
-    const firstName = data.get('firstName');
-    const lastName = data.get('lastName');
-    const jobTitle = data.get('jobTitle');
-    let dob = new Date(data.get('dob'));
-    const gender = data.get('gender');
-    
+    const firstName = data.get("firstName");
+    const lastName = data.get("lastName");
+    const jobTitle = data.get("jobTitle");
+    let dob = new Date(data.get("dob"));
+    const gender = data.get("gender");
 
     if (!dob) {
       dob = new Date();
     }
 
-    console.log(dob)
+    console.log(dob);
 
-    await connection.queryArray(`
+    await connection.queryArray(
+      `
       UPDATE
         person
       SET
@@ -68,16 +64,14 @@ routerPerson
       WHERE
         z_pk_person = $personId
       `,
-      { personId, firstName, lastName, gender, dob, jobTitle }
+      { personId, firstName, lastName, gender, dob, jobTitle },
     );
 
     const person = await getPerson(connection, personId);
 
-    context.response.body = r(<Person person={person} />)
-    
+    context.response.body = r(<Person person={person} />);
   })
-  .get('/edit-form', async (context) => {
-
+  .get("/edit-form", async (context) => {
     if (typeof context.params.personId !== "string") {
       context.response.body = "Person not found";
       context.response.status = 404;
@@ -88,7 +82,6 @@ routerPerson
     const person = await getPerson(connection, personId);
 
     context.response.body = render(<PersonForm person={person} />);
-
   });
 
 export default routerPerson;
