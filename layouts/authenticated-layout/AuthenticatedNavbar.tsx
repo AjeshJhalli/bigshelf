6517dfbead53ff81cac9f27aceea7b48bfcabe8c
Ajesh Jhalli <ajeshjhalli@mail.com>
2024-08-10 13:19:35 +0100

    Remove search

--- layouts/authenticated-layout/AuthenticatedNavbar.tsx
+++ layouts/authenticated-layout/AuthenticatedNavbar.tsx
@@ -57,21 +57,6 @@ export default function AuthenticatedNavbar(
         </li>
       </ul>
       <div className="navbar-end justify-end flex gap-x-6 px-6">
-        <label className="input input-bordered flex items-center gap-2">
-          <input type="search" className="grow" placeholder="Search not yet enabled" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
         <a className="btn btn-primary" href="/auth/sign-out">Sign Out</a>
       </div>
     </nav>
